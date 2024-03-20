d6820df059abde68f18ded6bf84ed83ea2871239
nsisong <edetnsisongpeter@gmail.com>
2024-03-20 01:09:52 +0100

    designed the s3 screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:nfc_manager/nfc_manager.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 import 'dart:convert';
+import 'my_home_page.dart'; // Importing MyHomePage widget
+import 'screen3.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,198 +13,162 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NFC Bluetooth Connection',
+      title: 'NFC Tag Reader',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: NFCBluetoothConnectionPage(),
+      home: NFCReaderPage(),
     );
   }
 }
 
-class NFCBluetoothConnectionPage extends StatefulWidget {
+class NFCReaderPage extends StatefulWidget {
   @override
-  _NFCBluetoothConnectionPageState createState() =>
-      _NFCBluetoothConnectionPageState();
+  _NFCReaderPageState createState() => _NFCReaderPageState();
 }
 
-class _NFCBluetoothConnectionPageState
-    extends State<NFCBluetoothConnectionPage> {
-  bool _isNFCEnabled = false;
-  bool _isScanning = false;
-  List<ScanResult> _scanResults = [];
+class _NFCReaderPageState extends State<NFCReaderPage> {
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  BluetoothDevice? _connectedDevice;
+  BluetoothCharacteristic? _writeCharacteristic;
 
   @override
   void initState() {
     super.initState();
     _initNFC();
-    requestPermissions();
-  }
-
-  Future<void> requestPermissions() async {
-    final bluetoothPermission = await Permission.bluetooth.request();
-    if (bluetoothPermission.isGranted) {
-      print('Bluetooth permission granted');
-      final scanPermission = await Permission.bluetoothScan.request();
-      if (scanPermission.isGranted) {
-        print('Bluetooth scan permission granted');
-        final connectPermission = await Permission.bluetoothConnect.request();
-        if (connectPermission.isGranted) {
-          print('Bluetooth connect permission granted');
-          // You can start scanning for devices or establish connections here.
-          _startScanning();
-        } else {
-          print('Bluetooth connect permission denied');
-        }
-      } else {
-        print('Bluetooth scan permission denied');
-      }
-    } else {
-      print('Bluetooth permission denied');
-    }
+    // scanForDevices();
   }
 
   Future<void> _initNFC() async {
     try {
-      await NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-        // Handle NFC tag discovery here
-        // Filter out the desired Bluetooth device by name based on tag info
-        List<ScanResult> filteredResults = _scanResults
-            .where((result) => result.device.name == 'NFC_UART')
-            .toList();
-
-        if (filteredResults.isNotEmpty) {
-          BluetoothDevice device = filteredResults.first.device;
-          // Connect to the Bluetooth device
-          await _connectToDevice(device);
-        } else {
-          print('NFC_UART device not found');
-        }
-      });
-      setState(() {
-        _isNFCEnabled = true;
-      });
+      await NfcManager.instance.startSession(
+        onDiscovered: (NfcTag tag) async {
+          String? bluetoothName = getBluetoothNameFromNfcTag(tag.data);
+          if (bluetoothName != null) {
+            await connectToBluetoothDevice(bluetoothName);
+          }
+          print('Bluetooth Name: $bluetoothName');
+        },
+      );
     } catch (e) {
       print('Error initializing NFC: $e');
     }
   }
 
-  Future<void> _startScanning() async {
-    setState(() {
-      _isScanning = true;
-    });
-    FlutterBlue flutterBlue = FlutterBlue.instance;
-    flutterBlue.scanResults.listen((List<ScanResult> results) {
-      setState(() {
-        _scanResults = results;
-      });
-    });
-    flutterBlue.startScan();
+  String? getBluetoothNameFromNfcTag(Map<String, dynamic> tagData) {
+    List<int> payload =
+        tagData['ndef']['cachedMessage']['records'][0]['payload'];
+    List<int> last15Numbers = payload.sublist(payload.length - 15);
+
+    String resultString = "";
+
+    for (int byte in last15Numbers) {
+      int asciiCode = byte;
+      String asciiChar = String.fromCharCode(asciiCode);
+
+      if (asciiCode >= 65 && asciiCode <= 122) {
+        resultString += asciiChar;
+      }
+    }
+
+    return resultString.isNotEmpty ? resultString : null;
   }
 
-  Future<void> _stopScanning() async {
-    setState(() {
-      _isScanning = false;
-    });
-    FlutterBlue flutterBlue = FlutterBlue.instance;
-    flutterBlue.stopScan();
+  Future<void> connectToBluetoothDevice(String bluetoothName) async {
+    try {
+      // Start scanning for devices
+      flutterBlue.startScan(timeout: Duration(seconds: 5));
+
+      // Listen to the scan results stream
+      flutterBlue.scanResults.listen((List<ScanResult> scanResults) {
+        for (ScanResult result in scanResults) {
+          if (result.device.name == bluetoothName) {
+            // Stop scanning once the device is found
+            flutterBlue.stopScan();
+            _connectedDevice = result.device;
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => DispenserControl()));
+            result.device.connect().then((_) async {
+              // Once connected, discover services and characteristics
+              List<BluetoothService> services =
+                  await result.device.discoverServices();
+              for (BluetoothService service in services) {
+                for (BluetoothCharacteristic characteristic
+                    in service.characteristics) {
+                  if (characteristic.properties.write) {
+                    _writeCharacteristic = characteristic;
+                    break;
+                  }
+                }
+              }
+            }).catchError((error) {
+              print('Failed to connect to device: $error');
+            });
+            break;
+          }
+        }
+      });
+    } catch (error) {
+      print('Error scanning for devices: $error');
+    }
+  }
+
+  void sendData(String data) {
+    if (_writeCharacteristic != null) {
+      List<int> bytes = utf8.encode(data + '\n');
+      _writeCharacteristic!.write(bytes);
+    }
+  }
+
+  @override
+  void dispose() {
+    NfcManager.instance.stopSession();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('NFC Bluetooth Connection'),
+        title: Text('Pay for fuel'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('NFC Enabled: $_isNFCEnabled'),
-            SizedBox(height: 20),
-            _isScanning
-                ? CircularProgressIndicator()
-                : Text('Scanning for NFC_UART device...'),
-            SizedBox(height: 20),
-            _buildScanResults(),
-          ],
-        ),
+      body: MyHomePage(), // Navigate to MyHomePage
+    );
+  }
+}
+
+class CircleTick extends StatelessWidget {
+  final double size;
+  final Color color;
+
+  const CircleTick({required this.size, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: Colors.green, width: 2),
+      ),
+      child: Stack(
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: size / 2,
+          ),
+          Positioned(
+            top: size * 0.25,
+            left: size * 0.25,
+            child: Icon(
+              Icons.check,
+              color: Colors.green,
+              size: size * 0.5,
+            ),
+          ),
+        ],
       ),
     );
   }
-
-  Widget _buildScanResults() {
-    if (_scanResults.isEmpty) {
-      return Text('No Bluetooth devices found');
-    } else {
-      return Column(
-        children: _scanResults.map((result) {
-          return ListTile(
-            title: Text(result.device.name ?? 'Unknown Device'),
-            subtitle: Text(result.device.id.toString()),
-            onTap: () {
-              _connectToDevice(result.device);
-            },
-          );
-        }).toList(),
-      );
-    }
-  }
-
-  Future<void> _connectToDevice(BluetoothDevice device) async {
-    try {
-      // Stop scanning before connecting
-      await _stopScanning();
-
-      // Connect to the selected Bluetooth device
-      await device.connect();
-
-      // Discover services and characteristics
-      List<BluetoothService> services = await device.discoverServices();
-
-      // Iterate through services and characteristics
-      services.forEach((service) {
-        service.characteristics.forEach((characteristic) {
-          // Set up notifications for all characteristics
-          characteristic.setNotifyValue(true);
-
-          // Send "HLO SN" to the device (optional)
-          characteristic.write(utf8.encode('HLO SN'));
-
-          // Listen for incoming messages
-          characteristic.value.listen((List<int> value) {
-            String incomingMessage = utf8.decode(value);
-            print('Incoming message: $incomingMessage');
-            // You can further process the incoming message here
-            // For example, parse the data based on a specific format
-            // or update the UI to display the received message.
-          });
-        });
-      });
-
-      // Display a snackbar indicating successful connection
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Connected to ${device.name}'),
-        ),
-      );
-    } catch (e) {
-      // Handle connection errors
-      print('Error connecting to device: $e');
-      // You may want to display an error message to the user
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Failed to connect to ${device.name}'),
-        ),
-      );
-    }
-  }
-
-  // Future<void> receive_Data() async {
-  //   if (targetCharacteristic != null) {
-  //     List<int> value = await targetCharacteristic!.read();
-  //     String receivedData = utf8.decode(value);
-  //     print('Received data: $receivedData');
-  //   }
-  // }
 }
---
+++ lib/screen3.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class DispenserControl extends StatefulWidget {
+  @override
+  _DispenserControlState createState() => _DispenserControlState();
+}
+
+class _DispenserControlState extends State<DispenserControl> {
+  String dispenserId = "6746179";
+  double currentScaleReading = 50.0; // Liters
+  String enteredVolume = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          // title: Text("Dispenser Control"),
+          ),
+      body: Center(
+        child: SingleChildScrollView(
+          // Wrap your Column with SingleChildScrollView
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 20), // Add space above the "CONNECTED" text
+              Text(
+                "CONNECTED",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.green,
+                ),
+              ),
+              SizedBox(height: 100), // Add space below the "CONNECTED" text
+              Text(
+                "Dispenser ID:\n$dispenserId", // Use '\n' to create a line break
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center, // Align the text to the center
+              ),
+              SizedBox(height: 20),
+              Text(
+                "SCALE: \nLiters",
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: EdgeInsets.all(100),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Enter Volume (Liters)",
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      enteredVolume = value;
+                    });
+                  },
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement logic to send the entered volume to the dispenser (replace with your specific logic)
+                  print("Dispensing $enteredVolume liters...");
+                  // Clear the entered volume after sending
+                  setState(() {
+                    enteredVolume = "";
+                  });
+                },
+                child: Text("Proceed"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
